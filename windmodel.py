dd36aab9fe5c64e2bb112c1ff5142237c32c34b5
Matthew Iannucci <mpiannucci@gmail.com>
2017-05-06 12:41:58 -0400

    Add functioning wind model

--- windmodel.py
+++ windmodel.py
@@ -4,7 +4,7 @@ from location import Location
 
 class GFSWindModel(NOAAModel):
 
-    _base_gfs_url = 'http://nomads.ncep.noaa.gov:9090/dods/%[1]s/gfs%[2]s/%[1]s_%[3]s.ascii?time[%[7]d:%[8]d],ugrd10m[%[7]d:%[8]d][%[5]d][%[6]d],vgrd10m[%[7]d:%[8]d][%[5]d][%[6]d],gustsfc[%[7]d:%[8]d][%[5]d][%[6]d]'
+    _base_gfs_url = 'http://nomads.ncep.noaa.gov:9090/dods/{0}/gfs{1}/{0}_{2}.ascii?time[{6}:{7}],ugrd10m[{6}:{7}][{4}][{5}],vgrd10m[{6}:{7}][{4}][{5}],gustsfc[{6}:{7}][{4}][{5}]'
 
     def create_ascii_url(self, location, start_time_index, end_time_index):
         timestamp = self.latest_model_time()
@@ -12,10 +12,13 @@ class GFSWindModel(NOAAModel):
         hourstring = timestamp.strftime('%Hz')
 
         lat_index, lon_index = self.location_index(location)
+        alt_index = self.altitude_index(location.altitude)
+        url = self._base_gfs_url.format(datestring, hourstring, alt_index, lat_index, lon_index, start_time_index, end_time_index)
+        return url
 
 class NAMWindModel(NOAAModel):
 
-    _base_nam_url = 'http://nomads.ncep.noaa.gov:9090/dods/nam/nam%[2]s/%[1]s_%[3]s.ascii?time[%[7]d:%[8]d],ugrd10m[%[7]d:%[8]d][%[5]d][%[6]d],vgrd10m[%[7]d:%[8]d][%[5]d][%[6]d],gustsfc[%[7]d:%[8]d][%[5]d][%[6]d]'
+    _base_nam_url = 'http://nomads.ncep.noaa.gov:9090/dods/nam/nam{1}/{0}_{2}.ascii?time[{6}:{7}],ugrd10m[{6}:{7}][{4}][{5}],vgrd10m[{6}:{7}][{4}][{5}],gustsfc[{6}:{7}][{4}][{5}]'
 
     def create_ascii_url(self, location, start_time_index, end_time_index):
         timestamp = self.latest_model_time()
@@ -23,3 +26,9 @@ class NAMWindModel(NOAAModel):
         hourstring = timestamp.strftime('%Hz')
 
         lat_index, lon_index = self.location_index(location)
+        alt_index = self.altitude_index(location.altitude)
+        url = self._base_nam_url.format(datestring, hourstring, alt_index, lat_index, lon_index, start_time_index, end_time_index)
+        return url
+
+def global_gfs_model():
+    return GFSWindModel('gfs_0p50', 'Global GFS 0.5 deg', Location(-90.00000, 0.00000), Location(90.0000, 359.5000), 0.5, 0.125, min_alt=1000.0, max_altitude=1.0, alt_res=21.717)
