7563339febfa2b7242c2971db28b99b47ab68dd1
Matthew Iannucci <mpiannucci@gmail.com>
2017-05-25 16:16:07 -0400

    Fix equality check in units module

--- units.py
+++ units.py
@@ -16,37 +16,37 @@ def degree_to_direction(degree):
 
 def direction_to_degree(direction):
     direction = direction.lower()
-    if direction is 'n' or direction is 'north':
+    if direction == 'n' or direction == 'north':
         return 0.0
-    elif direction is 'nne' or direction is 'north-northeast':
+    elif direction == 'nne' or direction == 'north-northeast':
         return 22.5
-    elif direction is 'ne' or direction is 'northeast':
+    elif direction == 'ne' or direction == 'northeast':
         return 45.0
-    elif direction is 'ene' or direction is 'east-northeast':
+    elif direction == 'ene' or direction == 'east-northeast':
         return 67.5
-    elif direction is 'e' or direction is 'east':
+    elif direction == 'e' or direction == 'east':
         return 90.0
-    elif direction is 'ese' or direction is 'east-southeast':
+    elif direction == 'ese' or direction == 'east-southeast':
         return 112.5
-    elif direction is 'se' or direction is 'southeast':
+    elif direction == 'se' or direction == 'southeast':
         return 135.0
-    elif direction is 'sse' or direction is 'south-southeast':
+    elif direction == 'sse' or direction == 'south-southeast':
         return 157.5
-    elif direction is 's' or direction is 'south':
+    elif direction == 's' or direction == 'south':
         return 180
-    elif direction is 'ssw' or direction is 'south-southwest':
+    elif direction == 'ssw' or direction == 'south-southwest':
         return 202.5
-    elif direction is 'sw' or direction is 'southwest':
+    elif direction == 'sw' or direction == 'southwest':
         return 225
-    elif direction is 'wsw' or direction is 'west-southwest':
+    elif direction == 'wsw' or direction == 'west-southwest':
         return 247.5
-    elif direction is 'w' or direction is 'west':
+    elif direction == 'w' or direction == 'west':
         return 270.0
-    elif direction is 'wnw' or direction is 'west-northwest':
+    elif direction == 'wnw' or direction == 'west-northwest':
         return 292.5
-    elif direction is 'nw' or direction is 'northwest':
+    elif direction == 'nw' or direction == 'northwest':
         return 315.0
-    elif direction is 'nnw' or direction is 'north-northwest':
+    elif direction == 'nnw' or direction == 'north-northwest':
         return 337.5
     else:
         return -1.0
@@ -63,48 +63,48 @@ class Measurement:
     pressure = 'pressure'
 
 def convert(value, measure, source_unit, dest_unit):
-    if source_unit is Units.metric:
-        if measure is Measurement.length:
-            if dest_unit is Units.english:
+    if source_unit == Units.metric:
+        if measure == Measurement.length:
+            if dest_unit == Units.english:
                 return value * 3.28
-        elif measure is Measurement.speed:
-            if dest_unit is Units.english:
+        elif measure == Measurement.speed:
+            if dest_unit == Units.english:
                 return value * 2.237
-            elif dest_unit is Units.knots:
+            elif dest_unit == Units.knots:
                 return value * 1.944
-        elif measure is Measurement.temperature:
-            if dest_unit is Units.english:
+        elif measure == Measurement.temperature:
+            if dest_unit == Units.english:
                 return (value * (9.0 / 5.0)) + 32.0
-        elif measure is Measurement.pressure:
-            if dest_unit is Units.english:
+        elif measure == Measurement.pressure:
+            if dest_unit == Units.english:
                 return value / 33.8638
-    elif source_unit is Units.english:
-        if measure is Measurement.length:
-            if dest_unit is Units.metric:
+    elif source_unit == Units.english:
+        if measure == Measurement.length:
+            if dest_unit == Units.metric:
                 return value / 3.28
-        elif measure is Measurement.speed:
-            if dest_unit is Units.metric:
+        elif measure == Measurement.speed:
+            if dest_unit == Units.metric:
                 return value / 2.237
-            elif dest_unit is Units.knots:
+            elif dest_unit == Units.knots:
                 return value / 1.15
-        elif measure is Measurement.temperature:
-            if dest_unit is Units.metric:
+        elif measure == Measurement.temperature:
+            if dest_unit == Units.metric:
                 return (value - 32.0) * (5.0 / 9.0)
-        elif measure is Measurement.pressure:
-            if dest_unit is Units.metric:
+        elif measure == Measurement.pressure:
+            if dest_unit == Units.metric:
                 return value * 33.8638
-    elif source_unit is Units.knots:
-        if measure is Measurement.speed:
-            if dest_unit is Units.metric:
+    elif source_unit == Units.knots:
+        if measure == Measurement.speed:
+            if dest_unit == Units.metric:
                 return value * 0.514
-            elif dest_unit is Units.english:
+            elif dest_unit == Units.english:
                 return value * 1.15
     return value
 
 def earths_radius(unit):
-    if unit is Units.metric:
+    if unit == Units.metric:
         return 6371.0
-    elif unit is Units.english:
+    elif unit == Units.english:
         return 3956.0
     else:
         return 1.0
