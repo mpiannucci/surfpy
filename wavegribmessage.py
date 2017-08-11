c7e9fd3cd1b41dcfb87e57231ffdde1203d5e565
Matthew Iannucci <mpiannucci@gmail.com>
2017-08-11 19:56:01 -0400

    Start work on wave grib wrapper

--- wavegribmessage.py
+++ wavegribmessage.py
@@ -1,1 +1,34 @@
 import grippy
+
+
+class WaveGribMessage(grippy.Message):
+
+    @property
+    def hour(self):
+        return self.sections[3].template.forecast_time
+
+    @property
+    def var(self):
+        return self.sections[self.PRODUCT_DEFINITION_SECTION_INDEX].template.parameter_number.abbrev
+
+    @property
+    def is_array_var(self):
+        return self.sections[self.PRODUCT_DEFINITION_SECTION_INDEX].template.first_fixed_surface_type_value == 241
+
+    @property
+    def var_index(self):
+        if not self.is_array_var:
+            return -1
+        return self.sections[self.PRODUCT_DEFINITION_SECTION_INDEX].template.first_fixed_surface_scaled_value
+
+    @property
+    def lat_indices(self):
+        return None
+
+    @property
+    def lon_indices(self):
+        return None
+
+    @property
+    def data(self):
+        return self.sections[self.DATA_SECTION_INDEX].all_scaled_values(self.sections[self.BITMAP_SECTION_INDEX].all_bit_truths)
