883a6352ed42efd037403ad079630cf250e7c88e
Matthew Iannucci <mpiannucci@gmail.com>
2021-05-08 16:09:04 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='surfpy',
-      version='0.96.0',
+      version='0.97.0',
       description='Wave and weather tools written in pure python',
       url='https://github.com/mpiannucci/surfpy',
       author='Matthew Iannucci',
