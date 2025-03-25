5edf82329888e60e590b94e4992d6b418cddecb1
stefanoscotti <stefanogscotti@gmail.com>
2025-03-24 23:45:16 -0400

    I snaked yo momma

--- surfdata.py
+++ surfdata.py
@@ -232,4 +232,4 @@ def buoy_data_to_json(wave_data):
     return wave_json
 
 # if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=5000)
+#     app.run(debug=True, host='0.0.0.0', port=5000) I snaked yo momma
