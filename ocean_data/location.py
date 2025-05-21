2c8f033498943849eba34e29323a61c0e013fc76
Martin Dietz <martinhdietz@gmail.com>
2025-05-21 11:08:17 -0400

    updated buoy for steamer lane

--- ocean_data/location.py
+++ ocean_data/location.py
@@ -16,7 +16,7 @@ LOCATION_TO_BUOYS = {
     "manasquan": {"swell": "44091", "met": "44009", "tide": "8533051"},
     "rockaways": {"swell": "44065", "met": "44009", "tide": "8516881"},
     "belmar": {"swell": "44091", "met": "44009", "tide": "8533051"},
-    "steamer_lane": {"swell": "46042", "met": "46042", "tide": "9413450"},
+    "steamer_lane": {"swell": "46236", "met": "46236", "tide": "9413450"},
     "trestles": {"swell": "46277", "met": "46277", "tide": "9410230"}
 }
 
